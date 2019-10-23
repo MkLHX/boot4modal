663a37199361a9414c1b1bab842482e7bad03149
MkLHX <mickael.lehoux@gmail.com>
2019-10-23 20:43:03 +0200

    [DOCS]render md in pre code tags

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,38 @@
 $(function () {
-    console.debug('ready!');
+    function decor() {
+        $('pre').each(function (i, p) {
+            if ($(p).attr('class') == undefined) {
+                $(p).attr('class', 'text-white');
+            }
+        });
+    }
+    function loadMd(path) {
+        return $.ajax(
+            {
+                url: path,
+            }
+        )
+    }
+    let getAlert = loadMd('https://mklhx.github.io/boot4modal/snippets/alert_only.md');
+    getAlert.done(function (md) {
+        $('#snippet-alert').find('pre').html(marked(md));
+        decor();
+    });
+
+    let getAlertCallback = loadMd('https://mklhx.github.io/boot4modal/snippets/alert_callback.md');
+    getAlertCallback.done(function (md) {
+        $('#snippet-alert-callback').find('pre').html(marked(md));
+        decor();
+    });
+
+    let getConfirm = loadMd('https://mklhx.github.io/boot4modal/snippets/confirm.md');
+    getConfirm.done(function (md) {
+        $('#snippet-confirm').find('pre').html(marked(md));
+        decor();
+    });
+
+
+
     $('#lnalert').on('click', function () {
         boot4.alert(
             {
@@ -48,12 +81,12 @@ $(function () {
     $('.copy').on('click', function (e) {
         let parent = e.target;
         let target = $('#install').find('pre').find('code');
-        target.after('<textarea class="d-none"></textarea>');
+        target.after('<textarea class="md-none"></textarea>');
         let txta = $('#install').find('textarea');
         txta.text(target.text());
         txta.select();
         target.addClass('bg-light text-dark');
         parent.
-        document.execCommand('copy');
+            document.execCommand('copy');
     });
 });
