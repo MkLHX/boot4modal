8713d78ecf8de9f68bf2f22f2a792ac5937332b8
MkLHX <mickael.lehoux@gmail.com>
2019-12-13 09:32:53 +0100

    allow labels colors and classes on both

--- boot4modal.js
+++ boot4modal.js
@@ -69,38 +69,40 @@ function Initial(properties, bootModalOptions) {
 	if (dialog.find(".modal-footer").length == 0) {
 		body.after(modalTemplate.footer);
 	}
-	if (properties.confirmBox != undefined) {
-		dialog.find(".modal-footer").html(modalTemplate.buttonConfirm);
-		if (properties.buttons_labels != undefined) {
-			if (properties.buttons_labels.cancel_btn != undefined) {
-				dialog.find(".boot4cancel").text(properties.buttons_labels.cancel_btn);
-			}
-			if (properties.buttons_labels.ok_btn != undefined) {
-				dialog.find(".boot4ok").text(properties.buttons_labels.ok_btn);
-			}
-		}
-		if (properties.buttons_colors != undefined) {
-			if (properties.buttons_colors.cancel_btn != undefined) {
-				dialog.find(".boot4cancel").removeClass('btn-light').css('background-color', properties.buttons_colors.cancel_btn);
-			}
-			if (properties.buttons_colors.ok_btn != undefined) {
-				dialog.find(".boot4ok").removeClass('btn-info').css('background-color', properties.buttons_colors.ok_btn);
-			}
-		}
-		if (properties.buttons_classes != undefined) {
-			if (properties.buttons_classes.cancel_btn != undefined) {
-				dialog.find(".boot4cancel").removeClass('btn-light').addClass(properties.buttons_classes.cancel_btn);
-			}
-			if (properties.buttons_classes.ok_btn != undefined) {
-				dialog.find(".boot4ok").removeClass('btn-info').addClass(properties.buttons_classes.ok_btn);
-			}
-		}
-	} else {
-		dialog.find(".modal-footer").html(modalTemplate.button);
-		if (properties.buttons_labels != undefined) {
-			dialog.find(".boot4ok").text(properties.buttons_labels.ok_btn);
-		}
-	}
+    if (properties.confirmBox != undefined) {
+        dialog.find(".modal-footer").html(modalTemplate.buttonConfirm);
+        if (properties.buttons_labels != undefined) {
+            if (properties.buttons_labels.cancel_btn != undefined) {
+                dialog.find(".boot4cancel").text(properties.buttons_labels.cancel_btn);
+            }
+        }
+        if (properties.buttons_colors != undefined) {
+            if (properties.buttons_colors.cancel_btn != undefined) {
+                dialog.find(".boot4cancel").removeClass('btn-light').css('background-color', properties.buttons_colors.cancel_btn);
+            }
+        }
+        if (properties.buttons_classes != undefined) {
+            if (properties.buttons_classes.cancel_btn != undefined) {
+                dialog.find(".boot4cancel").removeClass('btn-light').addClass(properties.buttons_classes.cancel_btn);
+            }
+        }
+    } else {
+        dialog.find(".modal-footer").html(modalTemplate.button);
+    }
+
+    if (properties.buttons_labels != undefined) {
+        dialog.find(".boot4ok").text(properties.buttons_labels.ok_btn);
+    }
+    if (properties.buttons_colors != undefined) {
+        if (properties.buttons_colors.ok_btn != undefined) {
+            dialog.find(".boot4ok").removeClass('btn-info').css('background-color', properties.buttons_colors.ok_btn);
+        }
+    }
+    if (properties.buttons_classes != undefined) {
+        if (properties.buttons_classes.ok_btn != undefined) {
+            dialog.find(".boot4ok").removeClass('btn-info').addClass(properties.buttons_classes.ok_btn);
+        }
+    }
 	dialog.find(".modal-body").html(tmpprops);
 	if (properties.size != undefined) {
 		switch (properties.size) {
